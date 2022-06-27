d2fea4850c8144336c104c12fc1f033dde57e8de
Razbuer <Rusyaevmax@yandex.ru>
2022-06-28 01:52:34 +0300

    update

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,3 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HippodromeTest {
+
+    /**
+     * Проверить, что при передаче в конструктор null, будет выброшено IllegalArgumentException;
+     * */
+    @Test
+    public void paramOfConstructorShouldNotBeNullAndShouldThrowException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    /**
+     * Проверить, что при передаче в конструктор первым параметром null,
+     * выброшенное исключение будет содержать сообщение "Name cannot be null.".
+     * Для этого нужно получить сообщение из перехваченного исключения и воспользоваться методом assertEquals;
+     */
+    @Test
+    public void paramOfConstructorShouldNotBeNullAndThrowShouldContainOurText() {
+
+    }
 
 }
